c9285c944a228ec85449e7dc5fa4d1e6045e28c5
brpetrov <63782101+brpetrov@users.noreply.github.com>
2020-06-16 11:52:49 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function() {
+  let disabled = document.querySelector('#submit').disabled;
+  let disabledClass = document.querySelector('#submit').classList;
 
-document.querySelector('form').onsubmit=function(){
-  const task=document.querySelector('#task').value;
-  const li=document.createElement('li');
-  li.innerHTML=task;
+  // By deafault the buttons is disabled
+  disabled = true;
+  disabledClass.add('disabled');
 
-document.querySelector('#tasks').append(li);
-document.querySelector('#task').value='';
+  document.querySelector('#task').onkeyup = function() {
+    if (document.querySelector('#task').value.length > 0) {
+      disabled = false;
+      disabledClass.remove('disabled');
+    } else {
+      disabled = true;
+      disabledClass.add('disabled');
+    }
+  }
 
-  return false;
+
+  document.querySelector('form').onsubmit = function() {
+    const task = document.querySelector('#task').value;
+    const li = document.createElement('li');
+    li.innerHTML = task;
 
 
-}
+    disabled = true;
+    disabledClass.add('disabled');
+    document.querySelector('#tasks').append(li);
+    document.querySelector('#task').value = '';
+
+    return false;
+
+
+  }
 
 })
